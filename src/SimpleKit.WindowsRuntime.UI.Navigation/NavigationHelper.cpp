2ecb03ccecab610d564a2bf783abb26ac35b449c
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-05-29 00:43:30 -0500

    Added delegates for Loaded and Unloaded events

--- src/SimpleKit.WindowsRuntime.UI.Navigation/NavigationHelper.cpp
+++ src/SimpleKit.WindowsRuntime.UI.Navigation/NavigationHelper.cpp
@@ -10,5 +10,37 @@ namespace winrt::SimpleKit::WindowsRuntime::UI::Navigation::implementation
 {
 	NavigationHelper::NavigationHelper(Page const& page)
 	{
+		m_page = page;
+
+		m_loadedToken = m_page.Loaded
+		(
+			winrt::auto_revoke,
+			{ this, &NavigationHelper::OnPageLoaded }
+		);
+
+		m_unloadedToken = m_page.Unloaded
+		(
+			winrt::auto_revoke,
+			{ this, &NavigationHelper::OnPageUnloaded }
+		);
+	}
+
+	NavigationHelper::~NavigationHelper()
+	{
+		m_revokeEvents();
+	}
+
+	void NavigationHelper::m_revokeEvents()
+	{
+		m_loadedToken.revoke();
+		m_unloadedToken.revoke();
+	}
+
+	void NavigationHelper::OnPageLoaded(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args)
+	{
+	}
+
+	void NavigationHelper::OnPageUnloaded(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args)
+	{
 	}
 }
--- src/SimpleKit.WindowsRuntime.UI.Navigation/NavigationHelper.h
+++ src/SimpleKit.WindowsRuntime.UI.Navigation/NavigationHelper.h
@@ -7,6 +7,27 @@ namespace winrt::SimpleKit::WindowsRuntime::UI::Navigation::implementation
 	struct NavigationHelper : NavigationHelperT<NavigationHelper>
 	{
 		NavigationHelper(Windows::UI::Xaml::Controls::Page const& page);
+		~NavigationHelper();
+
+	private:
+		Windows::UI::Xaml::Controls::Page m_page;
+
+		void m_revokeEvents();
+
+		Windows::UI::Xaml::Controls::Page::Loaded_revoker m_loadedToken;
+		Windows::UI::Xaml::Controls::Page::Unloaded_revoker m_unloadedToken;
+
+		void OnPageLoaded
+		(
+			Windows::Foundation::IInspectable const& sender,
+			Windows::UI::Xaml::RoutedEventArgs const& args
+		);
+
+		void OnPageUnloaded
+		(
+			Windows::Foundation::IInspectable const& sender,
+			Windows::UI::Xaml::RoutedEventArgs const& args
+		);
 	};
 }
 
