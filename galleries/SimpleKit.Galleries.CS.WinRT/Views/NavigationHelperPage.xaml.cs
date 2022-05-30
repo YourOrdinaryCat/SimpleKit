d22de423e882cb7a6e913cd6c6ef8206bc38d8eb
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-05-30 11:27:27 -0500

    Added initial NavigationHelper page

---
+++ galleries/SimpleKit.Galleries.CS.WinRT/Views/NavigationHelperPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using SimpleKit.WindowsRuntime.UI.Navigation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SimpleKit.Galleries.CS.WinRT.Views
+{
+    /// <summary>
+    /// A page showcasing NavigationHelper's capabilities
+    /// that explains how the gestures work.
+    /// </summary>
+    public sealed partial class NavigationHelperPage : Page
+    {
+        private readonly NavigationHelper _navigationHelper;
+
+        public NavigationHelperPage()
+        {
+            this.InitializeComponent();
+
+            _navigationHelper = new NavigationHelper(this);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
