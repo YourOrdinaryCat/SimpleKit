74c5a9110851ce7499910460f4bce51788d6f782
Omar Plata Salas <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-05-29 22:47:35 -0500

    Added RelayCommand helper class
    Add RelayCommand in WinRT Input project (#2)
    
    * Added WinRT UI Input project
    
    * Added RelayCommand class
    
    * Added webhosthidden attribute
    
    * Added initial delegates
    
    * Added CanExecuteChanged event

---
+++ src/SimpleKit.WindowsRuntime.UI.Input/RelayCommand.cpp
@@ -0,0 +1,51 @@
+﻿#include "pch.h"
+#include "RelayCommand.h"
+#if __has_include("RelayCommand.g.cpp")
+#include "RelayCommand.g.cpp"
+#endif
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Input::implementation
+{
+
+#pragma region Using Directives
+	using SimpleKit::WindowsRuntime::UI::Input::ExecuteDelegate;
+	using SimpleKit::WindowsRuntime::UI::Input::CanExecuteDelegate;
+
+	using Windows::Foundation::EventHandler;
+	using Windows::Foundation::IInspectable;
+#pragma endregion
+
+	RelayCommand::RelayCommand(ExecuteDelegate const& execute)
+	{
+		if (execute == nullptr)
+			throw hresult_invalid_argument(L"Provided ExecuteDelegate cannot be null.");
+		m_execute = execute;
+	}
+
+	RelayCommand::RelayCommand(ExecuteDelegate const& execute, CanExecuteDelegate const& canExecute)
+		: RelayCommand(execute)
+	{
+		m_canExecute = canExecute;
+	}
+
+	void RelayCommand::Execute(IInspectable const& param)
+	{
+		m_execute(param);
+	}
+
+	bool RelayCommand::CanExecute(IInspectable const& param)
+	{
+		if (m_canExecute != nullptr) return m_canExecute(param);
+		return true;
+	}
+
+	winrt::event_token RelayCommand::CanExecuteChanged(EventHandler<IInspectable> const& handler)
+	{
+		return m_canExecuteChanged.add(handler);
+	}
+
+	void RelayCommand::CanExecuteChanged(winrt::event_token const& token) noexcept
+	{
+		m_canExecuteChanged.remove(token);
+	}
+}
---
+++ src/SimpleKit.WindowsRuntime.UI.Input/RelayCommand.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "RelayCommand.g.h"
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Input::implementation
+{
+	struct RelayCommand : RelayCommandT<RelayCommand>
+	{
+		RelayCommand
+		(
+			SimpleKit::WindowsRuntime::UI::Input::ExecuteDelegate const& execute
+		);
+
+		RelayCommand
+		(
+			SimpleKit::WindowsRuntime::UI::Input::ExecuteDelegate const& execute,
+			SimpleKit::WindowsRuntime::UI::Input::CanExecuteDelegate const& canExecute
+		);
+
+		void Execute(winrt::Windows::Foundation::IInspectable const& param);
+		bool CanExecute(winrt::Windows::Foundation::IInspectable const& param);
+
+		winrt::event_token CanExecuteChanged(Windows::Foundation::EventHandler<Windows::Foundation::IInspectable> const& handler);
+		void CanExecuteChanged(winrt::event_token const& token) noexcept;
+
+	private:
+		winrt::event<Windows::Foundation::EventHandler<Windows::Foundation::IInspectable>> m_canExecuteChanged;
+
+		SimpleKit::WindowsRuntime::UI::Input::ExecuteDelegate m_execute;
+		SimpleKit::WindowsRuntime::UI::Input::CanExecuteDelegate m_canExecute;
+	};
+}
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Input::factory_implementation
+{
+	struct RelayCommand : RelayCommandT<RelayCommand, implementation::RelayCommand>
+	{
+	};
+}
