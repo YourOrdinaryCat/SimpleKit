8460f3769c7b112dd505935eea8e8f44738c99aa
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-05-30 00:08:07 -0500

    Added initial UWP app project

---
+++ galleries/SimpleKit.Galleries.CS.WinRT/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace SimpleKit.Galleries.CS.WinRT
+{
+    /// <summary>
+    /// The main gallery page, contains links to other
+    /// pages that show off the features of SimpleKit.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
