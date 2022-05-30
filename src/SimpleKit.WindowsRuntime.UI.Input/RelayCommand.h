0911d0a1d0fbb120da91caf67ca0b835a40f5e12
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-05-29 20:40:24 -0500

    Added RelayCommand class

---
+++ src/SimpleKit.WindowsRuntime.UI.Input/RelayCommand.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "RelayCommand.g.h"
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Input::implementation
+{
+	struct RelayCommand : RelayCommandT<RelayCommand>
+	{
+		RelayCommand() = default;
+	};
+}
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Input::factory_implementation
+{
+	struct RelayCommand : RelayCommandT<RelayCommand, implementation::RelayCommand>
+	{
+	};
+}
