65d50db62d72f271336bd439787ffbb7c8a9d502
Omar Plata Salas <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-05-31 13:35:49 -0500

    Added a C# gallery app (#3)
    Add a C# gallery app to showcase SimpleKit and its capabilities (#3)
    
    * Added initial UWP app project
    
    * Added project references
    
    * Added NavigationHelper to MainPage
    
    * Added simple button
    
    * Added initial NavigationHelper page
    
    * Improved code formatting
    
    * Added button click handlers
    
    * Improved views
    
    * Added discards and this
    
    * Updated project targets, added WinUI

---
+++ galleries/SimpleKit.Galleries.CS.WinRT/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using SimpleKit.Galleries.CS.WinRT.Views;
+using SimpleKit.WindowsRuntime.UI.Navigation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SimpleKit.Galleries.CS.WinRT
+{
+    /// <summary>
+    /// The main gallery page, contains links to other
+    /// pages that show off the features of SimpleKit.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private readonly NavigationHelper _navigationHelper;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this._navigationHelper = new NavigationHelper(this);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _ = this.Frame.Navigate(typeof(NavigationHelperPage));
+        }
+    }
+}
