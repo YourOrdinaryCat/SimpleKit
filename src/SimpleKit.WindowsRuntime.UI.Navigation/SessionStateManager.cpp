228664165792f77ec07023547da75a9bfbe08048
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-05-31 14:15:02 -0500

    Added SessionStateManager class

---
+++ src/SimpleKit.WindowsRuntime.UI.Navigation/SessionStateManager.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "SessionStateManager.h"
+#if __has_include("SessionStateManager.g.cpp")
+#include "SessionStateManager.g.cpp"
+#endif
+
+namespace winrt::SimpleKit.WindowsRuntime.UI.Navigation::implementation
+{
+    int32_t SessionStateManager::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void SessionStateManager::MyProperty(int32_t /*value*/)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ src/SimpleKit.WindowsRuntime.UI.Navigation/SessionStateManager.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "SessionStateManager.g.h"
+
+namespace winrt::SimpleKit.WindowsRuntime.UI.Navigation::implementation
+{
+    struct SessionStateManager : SessionStateManagerT<SessionStateManager>
+    {
+    };
+}
+
+namespace winrt::SimpleKit.WindowsRuntime.UI.Navigation::factory_implementation
+{
+    struct SessionStateManager : SessionStateManagerT<SessionStateManager, implementation::SessionStateManager>
+    {
+    };
+}
