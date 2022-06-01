e622ace5336290121f73e82a847c338b5396ec0a
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-06-01 11:46:56 -0500

    Added method to register frames with needed properties

--- src/SimpleKit.WindowsRuntime.UI.Navigation/SessionStateManager.cpp
+++ src/SimpleKit.WindowsRuntime.UI.Navigation/SessionStateManager.cpp
@@ -6,4 +6,30 @@
 
 namespace winrt::SimpleKit::WindowsRuntime::UI::Navigation::implementation
 {
+
+#pragma region Using Directives
+	using Windows::UI::Xaml::DependencyProperty;
+	using Windows::UI::Xaml::Controls::Frame;
+#pragma endregion
+
+	void SessionStateManager::RegisterFrame(Frame const& frame, hstring const& sessionStateKey, hstring const& sessionBaseKey)
+	{
+		if (frame.GetValue(m_frameSessionStateKeyProperty) != nullptr)
+		{
+			throw hresult_invalid_argument(L"Frames can only be registered to one session state key.");
+		}
+
+		if (frame.GetValue(m_frameSessionStateProperty) != nullptr)
+		{
+			throw hresult_illegal_method_call(L"Frames must either be registered before accessing frame session state, or not registered at all.");
+		}
+
+		//TODO: Register the frame
+		throw hresult_not_implemented();
+	}
+
+	std::vector<weak_ref<Frame>> SessionStateManager::m_registeredFrames;
+
+	DependencyProperty SessionStateManager::m_frameSessionStateProperty{ nullptr };
+	DependencyProperty SessionStateManager::m_frameSessionStateKeyProperty{ nullptr };
 }
--- src/SimpleKit.WindowsRuntime.UI.Navigation/SessionStateManager.h
+++ src/SimpleKit.WindowsRuntime.UI.Navigation/SessionStateManager.h
@@ -4,14 +4,21 @@
 
 namespace winrt::SimpleKit::WindowsRuntime::UI::Navigation::implementation
 {
-    struct SessionStateManager : SessionStateManagerT<SessionStateManager>
-    {
-    };
+	struct SessionStateManager : SessionStateManagerT<SessionStateManager>
+	{
+		static void RegisterFrame(Windows::UI::Xaml::Controls::Frame const& frame, hstring const& sessionStateKey, hstring const& sessionBaseKey);
+
+	private:
+		static std::vector<weak_ref<Windows::UI::Xaml::Controls::Frame>> m_registeredFrames;
+
+		static Windows::UI::Xaml::DependencyProperty m_frameSessionStateProperty;
+		static Windows::UI::Xaml::DependencyProperty m_frameSessionStateKeyProperty;
+	};
 }
 
 namespace winrt::SimpleKit::WindowsRuntime::UI::Navigation::factory_implementation
 {
-    struct SessionStateManager : SessionStateManagerT<SessionStateManager, implementation::SessionStateManager>
-    {
-    };
+	struct SessionStateManager : SessionStateManagerT<SessionStateManager, implementation::SessionStateManager>
+	{
+	};
 }
