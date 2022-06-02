efd4882b55b11438f27dfc5451027ceee57c02e4
Omar Plata Salas <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-06-02 18:34:20 +0000

    Added helpers for DataReader and DataWriter (#5)
     Add helpers for DataReader and DataWriter to WinRT Data project (#5)
    
    * Added WinRT Data project
    
    * Added DataReaderHelper and DataWriterHelper
    
    * Added supported stream types
    
    * Added methods to read and write strings
    
    * Added methods to read and write basic types
    
    * Added methods to read and write Maps
    
    * Added methods to read and write vectors
    
    * Added IVector support to WriteObject

---
+++ SimpleKit.WindowsRuntime.Data/DataReaderHelper.cpp
@@ -0,0 +1,114 @@
+﻿#include "pch.h"
+#include "DataReaderHelper.h"
+#if __has_include("DataReaderHelper.g.cpp")
+#include "DataReaderHelper.g.cpp"
+#endif
+
+using winrt::Windows::Foundation::IInspectable;
+using winrt::Windows::Foundation::PropertyType;
+
+using winrt::Windows::Foundation::Collections::IMap;
+using winrt::Windows::Foundation::Collections::IVector;
+
+using winrt::Windows::Storage::Streams::DataReader;
+
+namespace winrt::SimpleKit::WindowsRuntime::Data::implementation
+{
+	hstring DataReaderHelper::ReadString(DataReader const& reader)
+	{
+		int length = reader.ReadUInt32();
+		return reader.ReadString(length);
+	}
+
+	IInspectable DataReaderHelper::ReadObject(DataReader const& reader)
+	{
+		auto type = reader.ReadByte();
+		switch (type)
+		{
+		case (int)StreamTypes::NullPtrType:
+			return nullptr;
+		case (int)StreamTypes::UInt8Type:
+			return box_value(reader.ReadByte());
+		case (int)StreamTypes::UInt16Type:
+			return box_value(reader.ReadUInt16());
+		case (int)StreamTypes::UInt32Type:
+			return box_value(reader.ReadUInt32());
+		case (int)StreamTypes::UInt64Type:
+			return box_value(reader.ReadUInt64());
+		case (int)StreamTypes::Int16Type:
+			return box_value(reader.ReadInt16());
+		case (int)StreamTypes::Int32Type:
+			return box_value(reader.ReadInt32());
+		case (int)StreamTypes::Int64Type:
+			return box_value(reader.ReadInt64());
+		case (int)StreamTypes::SingleType:
+			return box_value(reader.ReadSingle());
+		case (int)StreamTypes::DoubleType:
+			return box_value(reader.ReadDouble());
+		case (int)StreamTypes::BooleanType:
+			return box_value(reader.ReadBoolean());
+		case (int)StreamTypes::Char16Type:
+			return box_value(reader.ReadUInt16());
+		case (int)StreamTypes::GuidType:
+			return box_value(reader.ReadGuid());
+		case (int)StreamTypes::StringType:
+			return box_value(ReadString(reader));
+		case (int)StreamTypes::MapStartMarker:
+			return box_value(ReadMap(reader));
+		case (int)StreamTypes::VectorStartMarker:
+			return box_value(ReadVector(reader));
+		default:
+			throw hresult_invalid_argument(L"Unsupported property type.");
+		}
+	}
+
+	IMap<IInspectable, IInspectable> DataReaderHelper::ReadMap(DataReader const& reader)
+	{
+		auto map = single_threaded_map<IInspectable, IInspectable>();
+		auto size = reader.ReadUInt32();
+
+		for (unsigned int index = 0; index < size; index++)
+		{
+			auto key = ReadObject(reader);
+			auto value = ReadObject(reader);
+
+			if (key)
+			{
+				map.Insert(key, value);
+			}
+			else
+			{
+				throw hresult_invalid_argument(L"Invalid key at index: " + index);
+			}
+		}
+
+		if (reader.ReadByte() != (uint8_t)StreamTypes::MapEndMarker)
+		{
+			throw hresult_invalid_argument(L"Invalid stream.");
+		}
+
+		return map;
+	}
+
+	IVector<IInspectable> DataReaderHelper::ReadVector(DataReader const& reader)
+	{
+		auto vector = single_threaded_vector<IInspectable>();
+		auto size = reader.ReadUInt32();
+
+		for (unsigned int index = 0; index < size; index++)
+		{
+			auto value = ReadObject(reader);
+			if (value)
+				vector.Append(value);
+			else
+				throw hresult_invalid_argument(L"Invalid key at index: " + index);
+		}
+
+		if (reader.ReadByte() != (uint8_t)StreamTypes::VectorEndMarker)
+		{
+			throw hresult_invalid_argument(L"Invalid stream.");
+		}
+
+		return vector;
+	}
+}
---
+++ SimpleKit.WindowsRuntime.Data/DataWriterHelper.cpp
@@ -0,0 +1,141 @@
+﻿#include "pch.h"
+#include "DataWriterHelper.h"
+#if __has_include("DataWriterHelper.g.cpp")
+#include "DataWriterHelper.g.cpp"
+#endif
+
+using winrt::Windows::Foundation::IInspectable;
+using winrt::Windows::Foundation::IPropertyValue;
+using winrt::Windows::Foundation::PropertyType;
+
+using winrt::Windows::Foundation::Collections::IMap;
+using winrt::Windows::Foundation::Collections::IVector;
+
+using winrt::Windows::Storage::Streams::DataWriter;
+
+namespace winrt::SimpleKit::WindowsRuntime::Data::implementation
+{
+	void DataWriterHelper::WriteString(DataWriter const& writer, hstring const& str)
+	{
+		writer.WriteByte((uint8_t)StreamTypes::StringType);
+		writer.WriteUInt32(writer.MeasureString(str));
+		writer.WriteString(str);
+	}
+
+	void DataWriterHelper::WriteObject(DataWriter const& writer, IInspectable const& obj)
+	{
+		if (obj == nullptr)
+		{
+			writer.WriteByte((uint8_t)StreamTypes::NullPtrType);
+			return;
+		}
+
+		auto prop = obj.try_as<IPropertyValue>();
+		if (prop != nullptr)
+		{
+			WriteProperty(writer, prop);
+			return;
+		}
+
+		auto map = obj.try_as<IMap<IInspectable, IInspectable>>();
+		if (map != nullptr)
+		{
+			WriteMap(writer, map);
+			return;
+		}
+
+		auto vector = obj.try_as<IVector<IInspectable>>();
+		if (vector != nullptr)
+		{
+			WriteVector(writer, vector);
+			return;
+		}
+
+		throw hresult_invalid_argument(L"Unsupported property type.");
+	}
+
+	void DataWriterHelper::WriteProperty(DataWriter const& writer, IPropertyValue const& propertyValue)
+	{
+		switch (propertyValue.Type())
+		{
+		case PropertyType::UInt8:
+			writer.WriteByte((uint8_t)StreamTypes::UInt8Type);
+			writer.WriteByte(propertyValue.GetUInt8());
+			return;
+		case PropertyType::UInt16:
+			writer.WriteByte((uint8_t)StreamTypes::UInt16Type);
+			writer.WriteUInt16(propertyValue.GetUInt16());
+			return;
+		case PropertyType::UInt32:
+			writer.WriteByte((uint8_t)StreamTypes::UInt32Type);
+			writer.WriteUInt32(propertyValue.GetUInt32());
+			return;
+		case PropertyType::UInt64:
+			writer.WriteByte((uint8_t)StreamTypes::UInt64Type);
+			writer.WriteUInt64(propertyValue.GetUInt64());
+			return;
+		case PropertyType::Int16:
+			writer.WriteByte((uint8_t)StreamTypes::Int16Type);
+			writer.WriteUInt16(propertyValue.GetInt16());
+			return;
+		case PropertyType::Int32:
+			writer.WriteByte((uint8_t)StreamTypes::Int32Type);
+			writer.WriteUInt32(propertyValue.GetInt32());
+			return;
+		case PropertyType::Int64:
+			writer.WriteByte((uint8_t)StreamTypes::Int64Type);
+			writer.WriteUInt64(propertyValue.GetInt64());
+			return;
+		case PropertyType::Single:
+			writer.WriteByte((uint8_t)StreamTypes::SingleType);
+			writer.WriteSingle(propertyValue.GetSingle());
+			return;
+		case PropertyType::Double:
+			writer.WriteByte((uint8_t)StreamTypes::DoubleType);
+			writer.WriteDouble(propertyValue.GetDouble());
+			return;
+		case PropertyType::Boolean:
+			writer.WriteByte((uint8_t)StreamTypes::BooleanType);
+			writer.WriteBoolean(propertyValue.GetBoolean());
+			return;
+		case PropertyType::Char16:
+			writer.WriteByte((uint8_t)StreamTypes::Char16Type);
+			writer.WriteUInt16(propertyValue.GetChar16());
+			return;
+		case PropertyType::Guid:
+			writer.WriteByte((uint8_t)StreamTypes::GuidType);
+			writer.WriteGuid(propertyValue.GetGuid());
+			return;
+		case PropertyType::String:
+			WriteString(writer, propertyValue.GetString());
+			return;
+		default:
+			throw hresult_invalid_argument(L"Unsupported property type.");
+		}
+	}
+
+	void DataWriterHelper::WriteMap(DataWriter const& writer, IMap<IInspectable, IInspectable> const& map)
+	{
+		writer.WriteByte((uint8_t)StreamTypes::MapStartMarker);
+		writer.WriteUInt32(map.Size());
+
+		for (auto&& pair : map)
+		{
+			WriteObject(writer, pair.Key());
+			WriteObject(writer, pair.Value());
+		}
+
+		writer.WriteByte((uint8_t)StreamTypes::MapEndMarker);
+	}
+
+	void DataWriterHelper::WriteVector(DataWriter const& writer, IVector<IInspectable> const& vector)
+	{
+		writer.WriteByte((uint8_t)StreamTypes::VectorStartMarker);
+		writer.WriteUInt32(vector.Size());
+
+		for (auto&& item : vector)
+			WriteObject(writer, item);
+
+		writer.WriteByte((uint8_t)StreamTypes::VectorEndMarker);
+	}
+}
