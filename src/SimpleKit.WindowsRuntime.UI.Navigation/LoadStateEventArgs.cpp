cca5e12b1469eefc573ae5dbe475f4a8c1c386b3
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-06-05 16:16:08 -0500

    Added event args for state loading

---
+++ src/SimpleKit.WindowsRuntime.UI.Navigation/LoadStateEventArgs.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "LoadStateEventArgs.h"
+#if __has_include("LoadStateEventArgs.g.cpp")
+#include "LoadStateEventArgs.g.cpp"
+#endif
+
+using winrt::Windows::Foundation::IInspectable;
+
+using winrt::Windows::Foundation::Collections::IMap;
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Navigation::implementation
+{
+	LoadStateEventArgs::LoadStateEventArgs(IInspectable navigationParameter, IMap<hstring, IInspectable> pageState)
+		: m_navigationParameter(navigationParameter), m_pageState(pageState)
+	{
+	}
+}
---
+++ src/SimpleKit.WindowsRuntime.UI.Navigation/LoadStateEventArgs.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "LoadStateEventArgs.g.h"
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Navigation::implementation
+{
+	struct LoadStateEventArgs : LoadStateEventArgsT<LoadStateEventArgs>
+	{
+		LoadStateEventArgs
+		(
+			Windows::Foundation::IInspectable navigationParameter,
+			Windows::Foundation::Collections::IMap<hstring, Windows::Foundation::IInspectable> pageState
+		);
+
+		Windows::Foundation::IInspectable NavigationParameter()
+		{
+			return m_navigationParameter;
+		}
+
+		Windows::Foundation::Collections::IMap<hstring, Windows::Foundation::IInspectable> PageState()
+		{
+			return m_pageState;
+		}
+
+	private:
+		Windows::Foundation::IInspectable m_navigationParameter;
+		Windows::Foundation::Collections::IMap<hstring, Windows::Foundation::IInspectable> m_pageState;
+	};
+}
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Navigation::factory_implementation
+{
+	struct LoadStateEventArgs : LoadStateEventArgsT<LoadStateEventArgs, implementation::LoadStateEventArgs>
+	{
+	};
+}
