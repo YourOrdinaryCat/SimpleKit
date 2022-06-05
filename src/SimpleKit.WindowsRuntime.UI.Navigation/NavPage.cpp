80f52fa8b03fbf7e7620aae83cd58886c19d0d61
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-06-05 15:37:09 -0500

    Added NavPage class

---
+++ src/SimpleKit.WindowsRuntime.UI.Navigation/NavPage.cpp
@@ -0,0 +1,12 @@
+﻿#include "pch.h"
+#include "NavPage.h"
+#if __has_include("NavPage.g.cpp")
+#include "NavPage.g.cpp"
+#endif
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Navigation::implementation
+{
+	NavPage::NavPage()
+	{
+	}
+}
---
+++ src/SimpleKit.WindowsRuntime.UI.Navigation/NavPage.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "NavPage.g.h"
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Navigation::implementation
+{
+	struct NavPage : NavPageT<NavPage>
+	{
+		NavPage();
+	};
+}
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Navigation::factory_implementation
+{
+	struct NavPage : NavPageT<NavPage, implementation::NavPage>
+	{
+	};
+}
