33475c6927c2891c5d881c46b63a0a8e1e37a042
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-06-09 11:58:17 -0500

    Added ExtendedTitleBar class

---
+++ src/SimpleKit.WindowsRuntime.UI.Controls.ExtendedTitleBar/ExtendedTitleBar.cpp
@@ -0,0 +1,13 @@
+﻿#include "pch.h"
+#include "ExtendedTitleBar.h"
+#if __has_include("ExtendedTitleBar.g.cpp")
+#include "ExtendedTitleBar.g.cpp"
+#endif
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Controls::implementation
+{
+	ExtendedTitleBar::ExtendedTitleBar()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/SimpleKit.WindowsRuntime.UI.Controls.ExtendedTitleBar/ExtendedTitleBar.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "ExtendedTitleBar.g.h"
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Controls::implementation
+{
+	struct ExtendedTitleBar : ExtendedTitleBarT<ExtendedTitleBar>
+	{
+		ExtendedTitleBar();
+	};
+}
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Controls::factory_implementation
+{
+	struct ExtendedTitleBar : ExtendedTitleBarT<ExtendedTitleBar, implementation::ExtendedTitleBar>
+	{
+	};
+}
