2b3988d6a204b997b43ab25ff50c864b6aece1bc
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-06-10 17:14:23 -0500

    Added title and icon properties

--- src/SimpleKit.WindowsRuntime.UI.Controls.ExtendedTitleBar/ExtendedTitleBar.cpp
+++ src/SimpleKit.WindowsRuntime.UI.Controls.ExtendedTitleBar/ExtendedTitleBar.cpp
@@ -4,10 +4,36 @@
 #include "ExtendedTitleBar.g.cpp"
 #endif
 
+using winrt::Windows::UI::Xaml::DependencyProperty;
+using winrt::Windows::UI::Xaml::PropertyMetadata;
+
 namespace winrt::SimpleKit::WindowsRuntime::UI::Controls::implementation
 {
 	ExtendedTitleBar::ExtendedTitleBar()
 	{
+		if (!m_TitleProperty)
+			m_registerProperties();
+
 		InitializeComponent();
 	}
+
+	void ExtendedTitleBar::m_registerProperties()
+	{
+		ExtendedTitleBar::m_TitleProperty =
+			DependencyProperty::Register(
+				L"Title",
+				xaml_typename<IInspectable>(),
+				xaml_typename<Controls::ExtendedTitleBar>(),
+				PropertyMetadata{ nullptr });
+
+		ExtendedTitleBar::m_IconProperty =
+			DependencyProperty::Register(
+				L"Icon",
+				xaml_typename<IInspectable>(),
+				xaml_typename<Controls::ExtendedTitleBar>(),
+				PropertyMetadata{ nullptr });
+	}
+
+	DependencyProperty ExtendedTitleBar::m_TitleProperty{ nullptr };
+	DependencyProperty ExtendedTitleBar::m_IconProperty{ nullptr };
 }
