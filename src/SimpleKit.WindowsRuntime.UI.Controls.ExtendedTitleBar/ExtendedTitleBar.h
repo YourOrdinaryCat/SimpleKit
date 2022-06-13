b23232f19954f197b008777887355a94a83e9497
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-06-13 16:20:00 -0500

    Reworked ExtendedTitleBar into a ContentControl

--- src/SimpleKit.WindowsRuntime.UI.Controls.ExtendedTitleBar/ExtendedTitleBar.h
+++ src/SimpleKit.WindowsRuntime.UI.Controls.ExtendedTitleBar/ExtendedTitleBar.h
@@ -2,19 +2,11 @@
 
 #include "ExtendedTitleBar.g.h"
 
-#include "../SimpleKit.WindowsRuntime.Shared/PropertyUtils.h"
-
 namespace winrt::SimpleKit::WindowsRuntime::UI::Controls::implementation
 {
 	struct ExtendedTitleBar : ExtendedTitleBarT<ExtendedTitleBar>
 	{
 		ExtendedTitleBar();
-
-		DEPENDENCY_PROPERTY(Windows::Foundation::IInspectable, Title);
-		DEPENDENCY_PROPERTY(Windows::Foundation::IInspectable, Icon);
-
-	private:
-		void m_registerProperties();
 	};
 }
 
