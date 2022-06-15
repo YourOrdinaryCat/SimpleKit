ca183a3f4aaa1e7a4bb5667b31f4c252473e9b34
Omar Plata Salas <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-06-15 17:44:12 +0000

    Added titlebar control (#7)
    Add a titlebar control (#7)
    
    * Added WinRT ExtendedTitleBar project
    
    * Added ExtendedTitleBar class
    
    * Added initial layout
    
    * Added title and icon properties
    
    * Made include syntax consistent
    
    * Reworked ExtendedTitleBar into a ContentControl
    
    * Improved default styling
    
    * Added method to use control as the current view's draggable area
    
    * Moved active/inactive window handling to VisualStates
    
    * Added custom brushes for titlebar foreground
    
    * Removed layout metrics event handler
    
    * Renamed resource, improved default styling
    
    * Implemented ExtendedTitleBar in C# gallery
    
    * Updated project target version

---
+++ src/SimpleKit.WindowsRuntime.UI.Controls.ExtendedTitleBar/ExtendedTitleBar.cpp
@@ -0,0 +1,80 @@
+﻿#include "pch.h"
+#include "ExtendedTitleBar.h"
+#if __has_include("ExtendedTitleBar.g.cpp")
+#include "ExtendedTitleBar.g.cpp"
+#endif
+
+using namespace winrt::Windows::ApplicationModel::Core;
+
+using namespace winrt::Windows::Foundation;
+
+using namespace winrt::Windows::UI;
+using namespace winrt::Windows::UI::Core;
+using namespace winrt::Windows::UI::ViewManagement;
+
+using namespace winrt::Windows::UI::Xaml;
+using namespace winrt::Windows::UI::Xaml::Media;
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Controls::implementation
+{
+	ExtendedTitleBar::ExtendedTitleBar()
+	{
+		DefaultStyleKey(winrt::box_value(this->GetRuntimeClassName()));
+		m_unloadedToken = Unloaded
+		(
+			{ this, &ExtendedTitleBar::OnUnloaded }
+		);
+	}
+
+	void ExtendedTitleBar::SetTitleBarForCurrentView()
+	{
+		auto view = ApplicationView::GetForCurrentView();
+		auto coreView = CoreApplication::GetCurrentView();
+		auto window = Window::Current();
+
+		auto titleBar = view.TitleBar();
+		auto coreTitleBar = coreView.TitleBar();
+
+		titleBar.ButtonBackgroundColor(Colors::Transparent());
+		titleBar.ButtonInactiveBackgroundColor(Colors::Transparent());
+
+		coreTitleBar.ExtendViewIntoTitleBar(true);
+		window.SetTitleBar(*this);
+
+		m_visibleChangedToken = coreTitleBar.IsVisibleChanged
+		(
+			winrt::auto_revoke,
+			{ this, &ExtendedTitleBar::OnVisibleChanged }
+		);
+
+		m_activatedToken = window.Activated
+		(
+			winrt::auto_revoke,
+			{ this, &ExtendedTitleBar::OnActivated }
+		);
+	}
+
+	void ExtendedTitleBar::OnVisibleChanged(CoreApplicationViewTitleBar const& sender, IInspectable const&)
+	{
+		if (sender.IsVisible())
+			VisualStateManager::GoToState(*this, L"TitleBarVisibleState", true);
+		else
+			VisualStateManager::GoToState(*this, L"TitleBarCollapsedState", true);
+	}
+
+	void ExtendedTitleBar::OnActivated(IInspectable const&, WindowActivatedEventArgs const& e)
+	{
+		if (e.WindowActivationState() == CoreWindowActivationState::Deactivated)
+			VisualStateManager::GoToState(*this, L"WindowInactiveState", true);
+		else
+			VisualStateManager::GoToState(*this, L"WindowActiveState", true);
+	}
+
+	void ExtendedTitleBar::OnUnloaded(IInspectable const&, RoutedEventArgs const&)
+	{
+		m_visibleChangedToken.revoke();
+		m_activatedToken.revoke();
+
+		Unloaded(m_unloadedToken);
+	}
+}
---
+++ src/SimpleKit.WindowsRuntime.UI.Controls.ExtendedTitleBar/ExtendedTitleBar.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "ExtendedTitleBar.g.h"
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Controls::implementation
+{
+	struct ExtendedTitleBar : ExtendedTitleBarT<ExtendedTitleBar>
+	{
+		ExtendedTitleBar();
+		void SetTitleBarForCurrentView();
+
+	private:
+		Windows::ApplicationModel::Core::CoreApplicationViewTitleBar::IsVisibleChanged_revoker m_visibleChangedToken;
+		void OnVisibleChanged
+		(
+			Windows::ApplicationModel::Core::CoreApplicationViewTitleBar const& sender,
+			Windows::Foundation::IInspectable const& args
+		);
+
+		Windows::UI::Xaml::Window::Activated_revoker m_activatedToken;
+		void OnActivated
+		(
+			Windows::Foundation::IInspectable const& sender,
+			Windows::UI::Core::WindowActivatedEventArgs const& e
+		);
+
+		winrt::event_token m_unloadedToken;
+		void OnUnloaded
+		(
+			Windows::Foundation::IInspectable const& sender,
+			Windows::UI::Xaml::RoutedEventArgs const& e
+		);
+	};
+}
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::Controls::factory_implementation
+{
+	struct ExtendedTitleBar : ExtendedTitleBarT<ExtendedTitleBar, implementation::ExtendedTitleBar>
+	{
+	};
+}
