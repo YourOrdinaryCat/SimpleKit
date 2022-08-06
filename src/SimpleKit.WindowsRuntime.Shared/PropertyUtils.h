f341f760613287cbde05da6f8dd6b9eefc40ee57
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-08-06 00:31:40 -0500

    Added macros for static and attached properties

--- src/SimpleKit.WindowsRuntime.Shared/PropertyUtils.h
+++ src/SimpleKit.WindowsRuntime.Shared/PropertyUtils.h
@@ -28,6 +28,34 @@ private:                         \
 	}                            \
 	type m_##name##;
 
+// Defines a static property for a WinRT class.
+#define STATIC_PROPERTY(type, name)     \
+public:                                 \
+	static type name()                  \
+	{                                   \
+		return m_##name##;              \
+	}                                   \
+	static void name(type const& value) \
+	{                                   \
+		m_##name## = value;             \
+	}                                   \
+private:                                \
+	static type m_##name##;
+
+// Defines a static get-only property for a WinRT class.
+#define STATIC_GET_PROPERTY(type, name) \
+public:                                 \
+	static type name()                  \
+	{                                   \
+		return m_##name##;              \
+	}                                   \
+private:                                \
+	static void name(type const& value) \
+	{                                   \
+		m_##name## = value;             \
+	}                                   \
+	static type m_##name##;
+
 // Defines a dependency property for a WinRT class.
 #define DEPENDENCY_PROPERTY(type, name)                                  \
 public:                                                                  \
@@ -44,4 +72,22 @@ public:                                                                  \
 		SetValue(m_##name##Property, winrt::box_value(value));           \
 	}                                                                    \
 private:                                                                 \
+	static winrt::Windows::UI::Xaml::DependencyProperty m_##name##Property;
+
+// Defines an attached property for a WinRT class.
+#define ATTACHED_PROPERTY(targetType, type, name)                             \
+public:                                                                       \
+	static winrt::Windows::UI::Xaml::DependencyProperty name##Property()      \
+	{                                                                         \
+		return m_##name##Property;                                            \
+	}                                                                         \
+	static type Get##name(targetType const& target)                           \
+	{                                                                         \
+		return winrt::unbox_value<type>(target.GetValue(m_##name##Property)); \
+	}                                                                         \
+	static void Set##name(targetType const& target, type const& value)        \
+	{                                                                         \
+		target.SetValue(m_##name##Property, winrt::box_value(value));         \
+	}                                                                         \
+private:                                                                      \
 	static winrt::Windows::UI::Xaml::DependencyProperty m_##name##Property;
