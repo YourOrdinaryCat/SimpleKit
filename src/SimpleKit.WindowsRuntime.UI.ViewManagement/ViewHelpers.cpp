d57666f14187bf285ab27df0abd58b4bbf87ef8b
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-08-09 16:06:32 -0500

    Added ViewHelpers class

---
+++ src/SimpleKit.WindowsRuntime.UI.ViewManagement/ViewHelpers.cpp
@@ -0,0 +1,9 @@
+﻿#include "pch.h"
+#include "ViewHelpers.h"
+#if __has_include("ViewHelpers.g.cpp")
+#include "ViewHelpers.g.cpp"
+#endif
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::ViewManagement::implementation
+{
+}
---
+++ src/SimpleKit.WindowsRuntime.UI.ViewManagement/ViewHelpers.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "ViewHelpers.g.h"
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::ViewManagement::implementation
+{
+	struct ViewHelpers : ViewHelpersT<ViewHelpers>
+	{
+		ViewHelpers() = default;
+	};
+}
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::ViewManagement::factory_implementation
+{
+	struct ViewHelpers : ViewHelpersT<ViewHelpers, implementation::ViewHelpers>
+	{
+	};
+}
