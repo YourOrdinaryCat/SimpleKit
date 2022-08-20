1d36913801861b07b8bdbba9fb9f70f915886dbc
Omar Plata Salas <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-08-20 00:07:27 +0000

    Added view management related features
    Add view management related features (#9)
    
    * Added WinRT view management project
    
    * Added ViewHelpers class
    
    * Added attached property to set an ApplicationView title on a page
    
    * Added method to show a new view
    
    * Added method to create a new ApplicationView instance
    
    * Improved nuspec, added docs, bumped version number

---
+++ src/SimpleKit.WindowsRuntime.UI.ViewManagement/ViewHelpers.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "ViewHelpers.g.h"
+
+#include <map>
+
+#include "PropertyUtils.h"
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::ViewManagement::implementation
+{
+	struct ViewHelpers : ViewHelpersT<ViewHelpers>
+	{
+		ViewHelpers() = default;
+
+		static Windows::Foundation::IAsyncOperation<Windows::UI::ViewManagement::ApplicationView>
+			CreateNewViewAsync(Windows::UI::Xaml::Interop::TypeName const sourcePageType);
+
+		static Windows::Foundation::IAsyncOperation<bool>
+			ShowNewViewAsync(Windows::UI::Xaml::Interop::TypeName const sourcePageType);
+
+		ATTACHED_PROPERTY(Windows::UI::Xaml::Controls::Page, hstring, ViewTitle)
+
+	private:
+		static std::map<int, winrt::weak_ref<Windows::UI::Xaml::Controls::Frame>> m_frames;
+		static std::map<int, winrt::event_token> m_tokens;
+
+		static void OnViewConsolidated(Windows::UI::ViewManagement::ApplicationView const& sender, Windows::UI::ViewManagement::ApplicationViewConsolidatedEventArgs const& args);
+		static void OnViewTitleChanged(Windows::UI::Xaml::DependencyObject const& d, Windows::UI::Xaml::DependencyPropertyChangedEventArgs const& e);
+	};
+}
+
+namespace winrt::SimpleKit::WindowsRuntime::UI::ViewManagement::factory_implementation
+{
+	struct ViewHelpers : ViewHelpersT<ViewHelpers, implementation::ViewHelpers>
+	{
+	};
+}
