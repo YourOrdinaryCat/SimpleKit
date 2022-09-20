0bcfb5f018ae2737bd6d70de5089b102c3784c19
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2022-09-19 20:07:19 -0500

    Added script to build individual projects

---
+++ BuildProject.ps1
@@ -0,0 +1,28 @@
+param
+(
+	[string]$ProjectName,
+	[switch]$CreatePackages = $false,
+	[string]$ProjectLog = "$($PSScriptRoot)\$($ProjectName)-out.txt",
+	[string]$NuGetSource
+)
+
+$projectPath = "$($PSScriptRoot)\src\$($ProjectName)"
+
+Remove-Item $ProjectLog
+$platforms = @("x86","x64","ARM","ARM64")
+
+ForEach ($platform in $platforms)
+{
+	devenv SimpleKit.sln /Build "Release|$($platform)" /Project $ProjectName /Out $ProjectLog
+}
+
+if ($CreatePackages)
+{
+	if (Test-Path -Path "$($projectPath)\NuGetManifest.nuspec")
+	{
+		Remove-Item $projectPath -Recurse -Include *.nupkg
+
+		nuget pack "$($projectPath)\NuGetManifest.nuspec" -OutputDirectory $projectPath
+		Get-ChildItem -Path $projectPath -Filter *.nupkg | ForEach { nuget add $_.FullName -source $NugetSource }
+	}
+}
