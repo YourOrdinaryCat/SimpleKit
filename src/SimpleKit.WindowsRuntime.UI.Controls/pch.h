f31c67bcd5335fd292b8675a49db0fdde1769457
YourOrdinaryCat <49221554+YourOrdinaryCat@users.noreply.github.com>
2023-02-07 22:25:18 -0500

    Removed unused precompiled headers

--- src/SimpleKit.WindowsRuntime.UI.Controls/pch.h
+++ src/SimpleKit.WindowsRuntime.UI.Controls/pch.h
@@ -4,7 +4,6 @@
 #include <winrt/Windows.ApplicationModel.Core.h>
 
 #include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.Foundation.Collections.h>
 
 #include <winrt/Windows.UI.h>
 #include <winrt/Windows.UI.Core.h>
@@ -13,7 +12,6 @@
 #include <winrt/Windows.UI.Xaml.h>
 #include <winrt/Windows.UI.Xaml.Interop.h>
 #include <winrt/Windows.UI.Xaml.Markup.h>
-#include <winrt/Windows.UI.Xaml.Media.h>
 
 #include <winrt/Windows.UI.Xaml.Controls.h>
 #include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
